4c8e4de0af04c927c2d29c6414662c7fa387bcd7
Damian Chlebda <damien.chlebda@gmail.com>
2020-05-02 14:51:30 +0200

    add FrogJump

---
+++ src/main/java/pl/chlebdad/FrogJump/FrogJump.java
@@ -0,0 +1,25 @@
+package pl.chlebdad.FrogJump;
+
+public class FrogJump {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(2, 6, 3));
+        System.out.println(solution(1, 6, 5));
+        System.out.println(solution(0, 6, -1));
+    }
+
+    public static int solution(int X, int Y, int D) {
+
+        if (X < 0 || Y < 0 || D <= 0) return 0;
+
+        int distanceToJump = Math.abs(X - Y);
+        int numberOfJumps = distanceToJump / D;
+
+        if (distanceToJump / D != 0) {
+            numberOfJumps++;
+        }
+
+        return numberOfJumps;
+    }
+}
