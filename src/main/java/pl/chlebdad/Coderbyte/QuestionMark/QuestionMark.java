e7cbbeef8150dc87b10a94babf7f733a1cc8822c
Damian Chlebda <damien.chlebda@gmail.com>
2020-07-24 12:00:05 +0200

    add Coderbyte free task - QuestionsMarks

--- src/main/java/pl/chlebdad/Coderbyte/QuestionMark/QuestionMark.java
+++ src/main/java/pl/chlebdad/Coderbyte/QuestionMark/QuestionMark.java
@@ -8,36 +8,31 @@ public class QuestionMark {
     public static void main(String[] args) {
 
         System.out.println(QuestionsMarks("arrb6???4xxbl5???eee5")); //true
-        System.out.println(QuestionsMarks("5??aaaaaaaaaaaaaaaaaaa?5?5")); //true
+        System.out.println(QuestionsMarks("5??aaaaaaaaaaaaaaaaaaa?5?5")); //false
         System.out.println(QuestionsMarks("9???1???9??1???9")); //false
-        System.out.println(QuestionsMarks("1?")); //false
+        System.out.println(QuestionsMarks("1???9")); //true
 
     }
 
     public static String QuestionsMarks(String str) {
-
-        str = str.replaceAll("[^0-9?]", "");
-        Pattern pattern = Pattern.compile("\\d\\?+\\d");
-        Matcher m = pattern.matcher(str);
-
+        String numMarks = str.replaceAll("[^0-9?]", "");
+        Pattern p = Pattern.compile("(\\d\\?+\\d)");
+        Matcher m = p.matcher(numMarks);
         boolean matched = false;
-
         while (m.find()) {
             String match = m.group();
             int a = Integer.parseInt(match.substring(0, 1));
             int b = Integer.parseInt(match.substring(match.length() - 1));
-
             if (a + b == 10) {
-
                 if (match.length() != 5) {
-                    matched = false;
-                    continue;
+                    return "false";
+                } else {
+                    matched = true;
+                    numMarks = numMarks.substring(1);
+                    m = p.matcher(numMarks);
                 }
-                matched = true;
             }
-
         }
-
         return matched ? "true" : "false";
     }
 
